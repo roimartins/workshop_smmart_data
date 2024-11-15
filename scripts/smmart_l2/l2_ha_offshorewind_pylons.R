c8ac19c443c7e61a6f89f88729e2c34fb1f8671b
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:51:28 +0000

    Update l2_ha_offshorewind_pylons.R

--- scripts/smmart_l2/l2_ha_offshorewind_pylons.R
+++ scripts/smmart_l2/l2_ha_offshorewind_pylons.R
@@ -22,3 +22,26 @@ owf_turb =  st_read(dsn = "./data/smmart_l1/smmart_l1_coastal_health_wk.gpkg", l
 owf_turb = owf_turb |> 
   mutate ( csquare =  getCsquareCode(lon,  lat, 0.05) ) |> 
   mutate ( csquare0p01 =  getCsquareCode(lon,  lat, 0.01) )
+
+
+
+
+#2.2  Get the CSquare grid for the L1 dataset
+
+
+#2.2.1 Get the CSquare grid that cover all extent of the  features in the L1 dataset
+
+
+grid = getCsquareGrid(spatial_layer = owf_turb, cell_size = 0.05,  projection = 3035 ) ## Albers European Equal Area projection
+
+
+
+
+#2.2.2 Get only  the CSquare grid that overlap with each  features in the L1 dataset
+
+
+grid_owf_turb = grid %>% filter ( csquare %in% unique(owf_turb$csquare ))
+
+str(owf_turb)
+
+
