d7f4f2c30d8eb9069769c8eb4e234ec840a0fc33
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:26:00 +0000

    Update l1_ha_offshore_windfarm_pylons.R

--- data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
+++ data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
@@ -10,10 +10,14 @@ library(sf)
 
 
 ##1.1 Data load  ###################################################################
-## Connect to Spatial Hub Portal to obtain the pylons data
+## Connect to GeoPackage to obtain the pylons data
 
 
-owf_turb =  st_read(dsn = "./data/smmart_l1/smmart_l1_coastal_health_wk.gpkg", layer = "l1_ha_offshorewind_pylons")
+
+owf_turb =  st_read(dsn = "./data/smmart_l0/smmart_l0_coastal_health_wk.gpkg", layer = "l1_ha_offshorewind_pylons")
+
+
+owf_turb
 
 
 ##1.2 Data Refinement   ###################################################################
@@ -31,4 +35,13 @@ a = pi*r^2 ## m^2
 
 owf_turb =  owf_turb |>
   mutate(rad = r , area = a) |> 
-  mutate ( lon = st_coordinates(owf_turb, )[,1 ], lat = st_coordinates(owf_turb)[,2] )
+  mutate ( lon = st_coordinates(owf_turb, )[,1 ],
+           lat = st_coordinates(owf_turb)[,2] )
+
+
+
+
+## 1.3 save_data  ###################################################################
+
+
+st_write(obj =  owf_turb   ,dsn = "./data/smmart_l1/smmart_l1_coastal_health_wk.gpkg", layer = "l1_ha_offshorewind_pylons")
