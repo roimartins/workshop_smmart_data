dc67ccfb57579768559b34a642e264c7971fb668
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:28:26 +0000

    Update l2_ha_offshorewind_pylons.R

--- scripts/smmart_l2/l2_ha_offshorewind_pylons.R
+++ scripts/smmart_l2/l2_ha_offshorewind_pylons.R
@@ -1,1 +1,17 @@
 
+library(tidyverse)
+library(sf)
+
+
+ 
+
+
+
+##############################################
+## 
+##  Level 2 - Integration of data and indicators into CSquare spatial framework 
+##
+###############################################
+
+
+## Load Level 1 Dataset - Level 1 dataset is created in L1_HA_offshorewind_pylons.R 
