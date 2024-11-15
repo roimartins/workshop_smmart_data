f96c39a01faf2e63246bea13bf3b2626dec62650
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:09:02 +0000

    Update l1_ha_offshore_windfarm_pylons.R

--- data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
+++ data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
@@ -11,3 +11,6 @@ library(sf)
 
 ##1.1 Data load  ###################################################################
 ## Connect to Spatial Hub Portal to obtain the pylons data
+
+
+owf_turb =  st_read(dsn = "./data/smmart_l1/smmart_l1_coastal_health_wk.gpkg", layer = "l1_ha_offshorewind_pylons")
