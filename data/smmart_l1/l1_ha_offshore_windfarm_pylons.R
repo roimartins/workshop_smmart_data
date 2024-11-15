361ee7647cb4c9b6c02f4369eb048f9ac52cf310
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:13:23 +0000

    Update l1_ha_offshore_windfarm_pylons.R

--- data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
+++ data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
@@ -14,3 +14,21 @@ library(sf)
 
 
 owf_turb =  st_read(dsn = "./data/smmart_l1/smmart_l1_coastal_health_wk.gpkg", layer = "l1_ha_offshorewind_pylons")
+
+
+##1.2 Data Refinement   ###################################################################
+
+
+
+##  Assign a buffer to each turbine location related to the radius of the turbine size. ###
+##  Average radio of the turbine is taken from a publication: 
+
+
+# Buffer distance determined by values in Foden et al. 2011
+r = 15 ## meters
+a = pi*r^2 ## m^2
+
+
+owf_turb =  owf_turb |>
+  mutate(rad = r , area = a) |> 
+  mutate ( lon = st_coordinates(owf_turb, )[,1 ], lat = st_coordinates(owf_turb)[,2] )
