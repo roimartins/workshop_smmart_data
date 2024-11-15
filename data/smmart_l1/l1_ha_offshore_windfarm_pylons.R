c40690098fd1683e024965c760057625dcc92e83
Roi Martinez <roimarful@gmail.com>
2024-11-15 13:05:16 +0000

    Create l1_ha_offshore_windfarm_pylons.R

---
+++ data/smmart_l1/l1_ha_offshore_windfarm_pylons.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(sf)
+
+
+##############################################
+## 
+##  Level 1 - Source data refinement
+##
+###############################################
+
+
+##1.1 Data load  ###################################################################
+## Connect to Spatial Hub Portal to obtain the pylons data
